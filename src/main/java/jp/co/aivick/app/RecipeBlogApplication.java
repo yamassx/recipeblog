d4e2aac836c48d0869481bfbd523a3d6e8c6ab24
yamassx <58455442+yamassx@users.noreply.github.com>
2020-06-13 22:06:44 +0900

    initial

---
+++ src/main/java/jp/co/aivick/app/RecipeBlogApplication.java
@@ -0,0 +1,13 @@
+package jp.co.aivick.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/aivick/app/Ajaxsample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.aivick.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ajaxsample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
