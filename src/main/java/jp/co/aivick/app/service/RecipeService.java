f7c4ffa444412c8594be500be80d083d4480a424
yamassx <58455442+yamassx@users.noreply.github.com>
2020-06-18 13:23:50 +0900

    recipeService

--- src/main/java/jp/co/aivick/app/service/RecipeService.java
+++ src/main/java/jp/co/aivick/app/service/RecipeService.java
@@ -1,5 +1,35 @@
 package jp.co.aivick.app.service;
 
-public class RecipeService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.aivick.app.dao.RecipeDao;
+import jp.co.aivick.app.entity.Recipe;
+
+@Service
+public class RecipeService {
+	@Autowired
+	private RecipeDao recipeDao;
+	
+	 public Recipe findBy(String recipe_id) {
+	        return recipeDao.find(recipe_id);
+	    }
+
+	    public List<Recipe> findAll() {
+	        return this.recipeDao.findAll();
+	    }
+
+	    @Transactional
+	    public Recipe create(Recipe recipe) {
+	        Recipe newRecipe = new Recipe();
+	        newRecipe.setRecipe_id(recipe.getRecipe_id());
+	        newRecipe.setName(recipe.getName());
+	        newRecipe.setDetail(recipe.getDetail());
+	        
+	        recipeDao.insert(newRecipe);
+	        return recipe;
+	    }	
 }
