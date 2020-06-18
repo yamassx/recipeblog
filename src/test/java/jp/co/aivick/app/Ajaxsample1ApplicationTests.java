046d1fb0ac0144879843842a25a7efc5fb4f77ee
yamassx <58455442+yamassx@users.noreply.github.com>
2020-06-18 12:42:28 +0900

    test remove

--- src/test/java/jp/co/aivick/app/Ajaxsample1ApplicationTests.java
+++ src/test/java/jp/co/aivick/app/Ajaxsample1ApplicationTests.java
@@ -1,13 +1,12 @@
-package jp.co.aivick.app;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Ajaxsample1ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+/*
+ * package jp.co.aivick.app;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class Ajaxsample1ApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
