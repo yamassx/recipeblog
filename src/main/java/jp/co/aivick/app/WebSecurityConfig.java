8285cdc79f63aa6495fd077ef88b65bda5a4d136
yamassx <58455442+yamassx@users.noreply.github.com>
2020-06-28 14:37:47 +0900

    画面遷移バグ修正

--- src/main/java/jp/co/aivick/app/WebSecurityConfig.java
+++ src/main/java/jp/co/aivick/app/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/signup/*", "/login", "/password/*", "/index","recipes/json", "/css/**","/layout/*", "/")
+			.antMatchers("/signup/*", "/login", "/password/*", "/index","recipes/json", "/css/**", "/js/**","layout/*", "/")
 			.permitAll()
 			.anyRequest()
 			.authenticated();
